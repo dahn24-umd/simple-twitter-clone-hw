703ea80886e0e8f950a266ae7c361a803184dd5e
David Ahn <davidahn1108@gmail.com>
2026-04-21 00:03:08 -0400

    attempted

--- src/tweet.tsx
+++ src/tweet.tsx
@@ -10,7 +10,34 @@ Also, if the post isn't liked already, then you want to display a white heart "�
 display a red heart "❤️" make sure to increase or decrease the like counter appropriately.
 Theres a 'tweet' class you can use to style your tweet.
 */
+function Tweet({ id, username, content, likes, timestamp }) {
+    const [likeStatus, likeClicked] = useState(false)
+    const [likeAmount, likeUpdate] = useState(likes)
 
+    const likeCnt = (() => {
+        if (likeStatus == false) {
+            likeUpdate(likeAmount + 1)
+        }
+        else {
+            likeUpdate(likeAmount - 1)
+        }
+        likeClicked(!likeStatus)
+    });
 
+    return (
+    <div className="tweet">
+        <h2>{username}</h2>
+        <p>{content}</p>
+        <p>{timestamp}</p>
+        
+        <div className="tweet">
+            <button onClick={likeCnt}>
+            {/* Conditional rendering for the heart icon */}
+            {likeStatus ? '❤️' : '🤍'} {likeAmount}
+            </button>
+        </div>
+    </div>
+    );
+};
 
-// export default Tweet;
+export default Tweet;
